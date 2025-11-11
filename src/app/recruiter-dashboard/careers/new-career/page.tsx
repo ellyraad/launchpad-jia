09d0e95a089460fe6fcdfce4bffbf38f0f6b74cd
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-11-11 18:17:46 +0800

    fix: resolve comments

--- src/app/recruiter-dashboard/careers/new-career/page.tsx
+++ src/app/recruiter-dashboard/careers/new-career/page.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { useSearchParams } from "next/navigation";
 import HeaderBar from "@/lib/PageComponent/HeaderBar";
-import CareerForm from "@/lib/components/CareerComponents/CareerForm"; // FIXME: delete after completing V2
 import CareerFormV2 from "@/lib/components/CareerComponents/CareerFormV2";
 
 export default function NewCareerPage() {
@@ -16,7 +15,6 @@ export default function NewCareerPage() {
         <div className="container-fluid mt--7" style={{ paddingTop: "6rem" }}>
           <div className="row">
             <CareerFormV2 formType="add" draftId={draftId} />
-            {/*<CareerForm formType="add" />*/}
           </div>
         </div>
       </>
