6271ff6e9f868113fb5f50e5f34939939184b957
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-11-12 10:48:19 +0800

    update super admin list

--- src/lib/SuperAdminUtils.tsx
+++ src/lib/SuperAdminUtils.tsx
@@ -1,10 +1,11 @@
 export const superAdminList = [
   "bryce.mercines@whitecloak.com",
   "vince.carandang@whitecloak.com",
-  "rafael.tiongson@whitecloak.com",
+  "rafa.tiongson@whitecloak.com",
   "donn.gamboa@whitecloak.com",
-  "anne.liango@whitecloak.com",
+  "anne.liangco@whitecloak.com",
   "miguel.fermin@whitecloak.com",
   "michelle.cruz@whitecloak.com",
   "anne.liangco@whitecloak.com",
+  "thea.juego@whitecloak.com",
 ];
